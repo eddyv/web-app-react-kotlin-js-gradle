4633ba80a0a7ff8d647973f01a92db06d9deed5f
eddyv <vaismanedward@gmail.com>
2022-02-27 13:49:23 -0500

    adding an external rest api to example. Males use of coroutines

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
 import kotlinx.browser.document
+import kotlinx.serialization.Serializable
 import react.create
 import react.dom.render
 
+@Serializable
 data class Video(val id: Int, val title: String, val speaker: String, val videoUrl: String)
+
 
 fun main() {
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
